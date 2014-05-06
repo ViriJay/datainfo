0e2a02d7c97fe16c0453c974357e5dbe48257e18
Viradj <viradj_13@hotmail.com>
2014-05-06 16:30:44 +0200

    2.2 niet helemaal af

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,6 +1,17 @@
 INSERT INTO F
 	(x, y, violation, motivation)
 	VALUES
-	("ABC", "EF", 0, "deze FD is een super key"),
+	("ABC", "EF", 0, "ABC is een super key"),
 	("E", "D", 1, "B, C, F zijn niet achterhaalbaar"),
 	("D", "A", 1, "A, B, C, E en F zijn niet achterhaalbaar");
+	
+INSERT INTO F1
+	(x, y, violation, motivation)
+	VALUES
+	("E", "D", 0, "E is een super key van D");
+	
+INSERT INTO F2
+	(x, y, violation, motivation)
+	VALUES
+	("ABC", "EF", 0, "ABC is een super key"),
+	("E", "A", 1, "E is geen super key");
