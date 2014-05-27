1bf296b96cb85e5513f7c50434fac62ed15ce977
Viradj <viradj_13@hotmail.com>
2014-05-27 16:35:52 +0200

    added exercise 2

---
+++ session5/excercise2cT2.sql
@@ -0,0 +1,8 @@
+SET TRANSACTION READ COMMITTED;
+BEGIN;
+--read2(x)
+SELECT titel FROM Boek WHERE isbn = '0136067018';
+--write2(x)
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = '0136067018';
+
+COMMIT;
